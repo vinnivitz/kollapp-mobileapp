a17ec7fff11b9f7b0854a53fd6121ce8df3999c8
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2024-11-22 11:30:56 +0100

    added persistence, mail and swagger config

--- orgaapp-backend/src/main/java/com/none/orgaappbackend/core/OrgaBackendApplication.java
+++ orgaapp-backend/src/main/java/com/none/orgaappbackend/core/OrgaBackendApplication.java
@@ -1,12 +1,26 @@
 package com.none.orgaappbackend.core;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrgaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrgaBackendApplication.class, args);
+	}
+
+	@Bean
+	OpenAPI openAPI() {
+		return new OpenAPI()
+				.components(new Components()
+						.addSecuritySchemes("bearer-key",
+								new SecurityScheme().type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT")));
 	}
 
 }
