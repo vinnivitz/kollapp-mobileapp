c89809b2efa5913b2198cfcb5979fdf5255f34ff
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2024-12-05 03:19:41 +0100

    Update test env

--- orgaapp-frontend/vite.config.ts
+++ orgaapp-frontend/vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
 	test: {
+		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
