d0b4922b97f0408b3294dcc213a2c7b46b5c99ca
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2025-01-11 16:38:12 +0100

    added spring component model to mapstruct mapper annotation in order to create a bean

--- kollapp-backend/src/main/java/com/none/kollappbackend/user/adapters/rest/mapper/KollappUserMapper.java
+++ kollapp-backend/src/main/java/com/none/kollappbackend/user/adapters/rest/mapper/KollappUserMapper.java
@@ -3,8 +3,10 @@ package com.none.kollappbackend.user.adapters.rest.mapper;
 import com.none.kollappbackend.user.adapters.rest.model.KollappUserTO;
 import com.none.kollappbackend.user.application.model.KollappUser;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Service;
 
-@Mapper
+@Mapper(componentModel = "spring")
+@Service
 public interface KollappUserMapper {
     KollappUserTO userToUserTO(KollappUser user);
     KollappUser UserTOToUser(KollappUserTO userTO);
