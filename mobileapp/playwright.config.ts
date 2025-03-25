820ab09c867b5dd70a9472e49b134e345447b24f
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-03-25 20:43:50 +0100

    Update playwright config

--- mobileapp/playwright.config.ts
+++ mobileapp/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	testDir: 'e2e',
+	testDir: 'test/e2e',
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 5174
