5c66fca9384a8558fd62e32c5be5f9305b0c0296
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-05-27 18:55:27 +0200

    Remove tailwindcss config

--- mobileapp/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-import typography from '@tailwindcss/typography';
-import scrollbarHide from 'tailwind-scrollbar-hide';
-
-export default {
-	content: ['./src/**/*.{svelte,ts}'],
-	plugins: [typography, scrollbarHide]
-} satisfies Config;
