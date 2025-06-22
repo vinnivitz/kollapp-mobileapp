23ece1d407b8708f537474a2547e22365c158d73
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-06-22 02:40:21 +0200

    Add version endpoint

---
+++ backend/src/main/java/org/kollappbackend/apiversion/adapters/primary/rest/VersionController.java
@@ -0,0 +1,36 @@
+package org.kollappbackend.apiversion.adapters.primary.rest;
+
+import org.jmolecules.architecture.hexagonal.PrimaryAdapter;
+import org.kollappbackend.apiversion.adapters.primary.rest.model.ApiVersionTO;
+import org.kollappbackend.core.adapters.primary.rest.model.DataResponseTO;
+import org.kollappbackend.core.adapters.primary.rest.model.ResponseTO;
+import org.kollappbackend.core.config.properties.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+
+@RestController
+@PrimaryAdapter
+@RequestMapping("/api/public/version")
+@AllArgsConstructor
+public class VersionController {
+
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping()
+    @Operation(summary = "Get the logged in user")
+    public ResponseEntity<ResponseTO> getVersion() {
+        ApiVersionTO apiVersionTO = new ApiVersionTO(applicationProperties.getVersion());
+        return ResponseEntity.ok(new DataResponseTO(apiVersionTO, "success.apiversion.get", messageSource));
+    }
+}
