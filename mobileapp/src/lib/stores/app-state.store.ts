65a2dae467bc7d331923e6494da5d53dfe5803c3
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-11-12 14:57:13 +0100

    Format

--- mobileapp/src/lib/stores/app-state.store.ts
+++ mobileapp/src/lib/stores/app-state.store.ts
@@ -36,20 +36,21 @@ function createAppStateStore(): AppStateStore {
 			await Promise.all([themeStore.init(), layoutStore.init(), localeStore.init()]);
 			if (dev) console.info('Core stores initialized.');
 
-		set(AppStateType.INITIALIZING_AUTH);
-		await authenticationStore.init();
-		if (dev) console.info('Authentication store initialized.');
+			set(AppStateType.INITIALIZING_AUTH);
+			await authenticationStore.init();
+			if (dev) console.info('Authentication store initialized.');
 
-		const isAuthenticated = !!get(authenticationStore);
+			const isAuthenticated = !!get(authenticationStore);
 
-		if (isAuthenticated) {
-			set(AppStateType.INITIALIZING_USER_DATA);
-			await Promise.all([userStore.init(), organizationStore.init()]);
-			if (dev) console.info('User data stores initialized.');
-		} else {
-			await Promise.all([userStore.reset(), organizationStore.reset()]);
-		}
-		connectionStore.init();			set(AppStateType.READY);
+			if (isAuthenticated) {
+				set(AppStateType.INITIALIZING_USER_DATA);
+				await Promise.all([userStore.init(), organizationStore.init()]);
+				if (dev) console.info('User data stores initialized.');
+			} else {
+				await Promise.all([userStore.reset(), organizationStore.reset()]);
+			}
+			connectionStore.init();
+			set(AppStateType.READY);
 		} catch (error) {
 			set(AppStateType.ERROR);
 			await showAlert('An error occurred during app initialization.', { type: AlertType.ERROR });
