a4d3af672ed6e13276d8708088c270a121c6a65b
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-11-21 23:33:59 +0100

    update goal name of plugin

--- backend/server/src/test/java/org/kollapp/core/BaseIT.java
+++ backend/server/src/test/java/org/kollapp/core/BaseIT.java
@@ -1,11 +1,12 @@
 package org.kollapp.core;
 
-import org.kollapp.KollappApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.kollapp.KollappApplication;
 
 @Testcontainers
 @SpringBootTest(classes = KollappApplication.class)
