c9dc73adbdbd12d72385d219e78dae6eea6f9a95
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-11-22 00:39:10 +0100

    remove early exit for no jwt secrets set

--- backend/server/src/main/java/org/kollapp/core/config/properties/JwtProperties.java
+++ backend/server/src/main/java/org/kollapp/core/config/properties/JwtProperties.java
@@ -1,7 +1,5 @@
 package org.kollapp.core.config.properties;
 
-import jakarta.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +7,6 @@ import lombok.Setter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
@@ -17,25 +14,13 @@ import org.springframework.validation.annotation.Validated;
 @NoArgsConstructor
 @Component
 @ConfigurationProperties(prefix = "kollapp.jwt")
-@Validated
 public class JwtProperties {
-    @NotBlank(message = "JWT auth secret must be configured")
     private String authSecret;
-
     private long authExpirationInSeconds;
-
-    @NotBlank(message = "JWT refresh secret must be configured")
     private String refreshSecret;
-
     private long refreshExpirationInSeconds;
-
-    @NotBlank(message = "JWT confirmation secret must be configured")
     private String confirmationSecret;
-
     private long confirmationExpirationInSeconds;
-
-    @NotBlank(message = "JWT reset password secret must be configured")
     private String resetPasswordSecret;
-
     private long resetPasswordExpirationInSeconds;
 }
