7ee5272ad40fb26414f558285358b468c5efc0a1
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-12-17 03:32:16 +0100

    Change border of pending members card to warning color

--- mobileapp/src/routes/organization/members/+page.svelte
+++ mobileapp/src/routes/organization/members/+page.svelte
@@ -266,7 +266,7 @@
 <!-- Snippets-->
 
 {#snippet pendingMembersCard()}
-	<Card title={$t('routes.organization.members.page.card.pending-members.title')} border="secondary" classList="mt-5">
+	<Card title={$t('routes.organization.members.page.card.pending-members.title')} border="warning" classList="mt-5">
 		{#each pendingMembers as member (member.id)}
 			{@render pendingMemberItem(member)}
 		{/each}
