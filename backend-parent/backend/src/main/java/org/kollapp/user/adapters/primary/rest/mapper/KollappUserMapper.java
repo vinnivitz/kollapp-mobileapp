fed8b93ec3446e4a003f7bb17372fa6a8cc140ae
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2025-12-22 00:39:21 +0100

    Remove wrong mappings

--- backend-parent/backend/src/main/java/org/kollapp/user/adapters/primary/rest/mapper/KollappUserMapper.java
+++ backend-parent/backend/src/main/java/org/kollapp/user/adapters/primary/rest/mapper/KollappUserMapper.java
@@ -1,7 +1,6 @@
 package org.kollapp.user.adapters.primary.rest.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.stereotype.Service;
 
 import org.kollapp.user.adapters.primary.rest.dto.KollappUserTO;
@@ -10,7 +9,5 @@ import org.kollapp.user.application.model.KollappUser;
 @Mapper(componentModel = "spring")
 @Service
 public interface KollappUserMapper {
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     KollappUserTO kollappUserToKollappUserTO(KollappUser user);
 }
