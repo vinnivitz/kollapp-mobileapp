8ca0131bcd097b3bf725b352064ca6e13a192d43
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2025-12-23 10:43:34 +0100

    #237: changed the name of the SystemRole transfer object class to a corresponding name

--- backend-parent/backend/src/main/java/org/kollapp/user/adapters/primary/rest/dto/enums/SystemRoleTO.java
+++ backend-parent/backend/src/main/java/org/kollapp/user/adapters/primary/rest/dto/enums/SystemRoleTO.java
@@ -4,17 +4,17 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 import org.kollapp.user.application.exception.InvalidSystemRoleException;
 
-public enum SystemRole {
+public enum SystemRoleTO {
     ROLE_KOLLAPP_ADMIN,
     ROLE_KOLLAPP_USER,
     ROLE_KOLLAPP_ORGANIZATION_MEMBER;
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public static SystemRole fromString(String value) {
+    public static SystemRoleTO fromString(String value) {
         if (value == null) {
             return null;
         }
-        for (SystemRole role : SystemRole.values()) {
+        for (SystemRoleTO role : SystemRoleTO.values()) {
             if (role.name().equals(value)) {
                 return role;
             }
